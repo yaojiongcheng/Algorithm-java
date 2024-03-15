deb1243395b31d5cc8561049ff99bf197adeaf60
yaojiongcheng <2564167401@qq.com>
2024-03-15 17:10:36 +0800

    新增双指针算法 - 判断子序列

--- src/com/jc/双指针算法/判断子序列/Main.java
+++ src/com/jc/双指针算法/判断子序列/Main.java
@@ -1,15 +1,38 @@
+package com.jc.双指针算法.判断子序列;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
 import java.util.Scanner;
 
+/*
+ * 双指针算法
+ * 判断子序列
+ * 请你判断 a 序列是否为 b 序列的子序列。
+ * */
 public class Main {
 
     public static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-
+        int n = nextInt();
+        int m = nextInt();
+        int[] arr = new int[n];
+        int[] brr = new int[m];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            brr[i] = nextInt();
+        }
+        int i = 0,j = 0;
+        while (i < n && j < m) {
+            if (arr[i] == brr[j]) i++;
+            j ++;
+        }
+        if (i == n) System.out.println("Yes");
+        else System.out.println("No");
     }
 
     private static String nextString() throws Exception {
