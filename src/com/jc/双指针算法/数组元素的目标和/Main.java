3f0b3aec98240021b697c3c2a7839cf5cb3124b8
yaojiongcheng <2564167401@qq.com>
2024-03-15 16:50:54 +0800

    新增双指针算法 - 数组元素的目标和

--- src/com/jc/双指针算法/数组元素的目标和/Main.java
+++ src/com/jc/双指针算法/数组元素的目标和/Main.java
@@ -1,3 +1,5 @@
+package com.jc.双指针算法.数组元素的目标和;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
@@ -5,6 +7,9 @@ import java.util.Scanner;
 
 /*
  * 双指针算法
+ * 数组元素的目标和
+ * 给定两个升序排序的有序数组 A 和 B，以及一个目标值 x。
+ * 请你求出满足 A[i]+B[j]=x 的数对 (i,j)。
  * */
 public class Main {
 
@@ -12,7 +17,24 @@ public class Main {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-
+        int n = nextInt();
+        int m = nextInt();
+        int x = nextInt();
+        int[] arr = new int[n];
+        int[] brr = new int[m];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            brr[i] = nextInt();
+        }
+        for (int i = 0, j = m - 1; i < n; i++) {
+            while (j >= 0 && arr[i] + brr[j] > x) j--;
+            if (arr[i] + brr[j] == x) {
+                System.out.println(i + " " + j);
+                return;
+            }
+        }
     }
 
     private static String nextString() throws Exception {
