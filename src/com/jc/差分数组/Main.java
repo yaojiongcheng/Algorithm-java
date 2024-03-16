8d996f9317c6b61f7f2ce873ac5fc9d31859c8e1
yaojiongcheng <2564167401@qq.com>
2024-03-16 16:28:22 +0800

    新增 求差分数组

--- src/com/jc/差分数组/Main.java
+++ src/com/jc/差分数组/Main.java
@@ -6,13 +6,41 @@ import java.io.StreamTokenizer;
 import java.util.Scanner;
 
 
+/**
+ * 差分数组 d[i] = a[i] - a[i-1]
+ * 将 数组p 变为 数组t
+ * 每次只能选择连续的一段区间 +1/-1
+ * 求最小操作次数
+ */
 public class Main {
 
     public static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-
+        int n = nextInt();
+        int[] p = new int[n + 1];
+        int[] t = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            p[i] = nextInt();
+        }
+        for (int i = 1; i <= n; i++) {
+            t[i] = nextInt();
+        }
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = p[i] - t[i];
+        }
+        for (int i = n; i > 0; i--) {
+            ans[i] = ans[i] - ans[i - 1];
+        }
+        long res1 = 0;
+        long res2 = 0;
+        for (int i = 1; i <= n; i++) {
+            if (ans[i] > 0) res1 += ans[i];
+            else if (ans[i] < 0) res2 += Math.abs(ans[i]);
+        }
+        System.out.println(Math.max(res1, res2));
     }
 
     private static String nextString() throws Exception {
